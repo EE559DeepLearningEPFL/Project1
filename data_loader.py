6dc02dffee91ff086248adc97c517f6d111418c4
Jiaanzhu <58867544+Jiaanzhu@users.noreply.github.com>
2021-05-14 13:46:14 +0200

    data loader

--- data_loader.py
+++ data_loader.py
@@ -1,5 +1,12 @@
 # data_loader.py
 from torch.utils.data import TensorDataset, DataLoader
+import dlc_practical_prologue as prologue
+import torch
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
 
 def load_data(N=1000, batch_size=50, seed=42):
     # Load data
@@ -7,6 +14,12 @@ def load_data(N=1000, batch_size=50, seed=42):
     train_target = torch.nn.functional.one_hot(train_target)
     test_target = torch.nn.functional.one_hot(test_target)
     
+    train_input = train_input.to(device)
+    train_target = train_target.to(device)
+    train_classes = train_classes.to(device)
+    test_input = test_input.to(device)
+    test_target = test_target.to(device)
+    test_classes = test_classes.to(device)
     # Normalize data
     mean, std = train_input.mean(), train_input.std()
     train_input.sub_(mean).div_(std)
