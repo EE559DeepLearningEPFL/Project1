42fbc4ec5cad4a8acb10213e8d9d67f52aec5f87
Jiaanzhu <58867544+Jiaanzhu@users.noreply.github.com>
2021-05-22 21:41:18 +0200

    optimization of parameters

--- optimization.py
+++ optimization.py
@@ -12,6 +12,7 @@ from data_loader import *
 from train import *
 
 # initial models for tuning learning rate and batch size
+# [model name, if weight sharing, if auxiliary loss]
 Train_model = [[MLP, False, False],
               [SiameseMLP, True, False],
               [AuxMLP, False, True],
@@ -30,18 +31,22 @@ def lr_bs_tuning(Train_model):
     '''
     tuning the learning rate and batch size
     '''
-    accuracy = []
-    std = []
-    model_number = 0
+    accuracy = [] # list for accuracy
+    std = [] # list for std
+    model_number = 0 # index of model
+    
     for models in Train_model:
-        gammas = [5e-3, 1e-3, 5e-4, 1e-4]
-        batch_sizes = [8, 16, 32, 64, 128]
-        test_accuracies = torch.empty((len(gammas), len(batch_sizes)))
-        test_stds = torch.empty((len(gammas), len(batch_sizes)))
+        gammas = [5e-3, 1e-3, 5e-4, 1e-4] # learning rates
+        batch_sizes = [8, 16, 32, 64, 128] # batch sizes
+        
+        test_accuracies = torch.empty((len(gammas), len(batch_sizes))) # store accuracy
+        test_stds = torch.empty((len(gammas), len(batch_sizes))) # store std
 
         for j in range(len(gammas)):
             for k in range(len(batch_sizes)):
                 accurate = []
+                
+                # train 10 times for each condition
                 for i in range(10):
                     model = models[0]()
                     model.to(device)
@@ -49,15 +54,21 @@ def lr_bs_tuning(Train_model):
                     loss = train(model, train_loader, gammas[j], 0, 25, verbose=False, siamese=models[1], aux=models[2])
                     tr_accuracy, te_accuracy = accu(model, train_loader, test_loader, siamese=models[1], aux=models[2])
                     accurate.append(te_accuracy)
+                    
                 test_accuracies[j,k] =  torch.FloatTensor(accurate).mean()
                 test_stds[j,k] =  torch.FloatTensor(accurate).std()
+                
         accuracy.append(test_accuracies)
         std.append(test_stds)
-        max_index = test_accuracies.argmax() 
+        
+        max_index = test_accuracies.argmax() # looking for the maximum accuracy
+        
         model_number += 1
+        # Print out results
         print('The optimal parameters for model_%d: learning rate %.5f, batch size: %d' %(model_number, gammas[(max_index)//5], batch_sizes[(max_index+1)%5-1]))
         
 # models for tuning auxiliary loss, learning rate and batch size based on the previous tuning step
+# [model name, if weight sharing, if auxiliary loss, learning rate, batch size]
 Train_auxmodel = [[AuxMLP, False, True, 5e-3, 16],
               [AuxsiameseMLP, True, True, 5e-3, 8],
               [AuxCNN, False, True, 5e-4, 8],
@@ -65,33 +76,42 @@ Train_auxmodel = [[AuxMLP, False, True, 5e-3, 16],
               [AuxResNet, False, True, 1e-3, 8],
               [AuxsiameseResNet, True, True, 5e-3, 8]]
 
+
+
 def alpha_tuning(Train_auxmodel):
     '''
     tuning the auxiliary loss coefficience alpha
     '''
-    accuracy_aux = []
-    std_aux = []
-    model_number_aux = 0
+    accuracy_aux = [] # list for accuracy
+    std_aux = [] #list for std
+    model_number_aux = 0 # index of model
+    
     for models in Train_auxmodel:
-        test_accuracies_aux = torch.empty((1, 11))
-        test_stds_aux = torch.empty((1,11))    
+        test_accuracies_aux = torch.empty((1, 11)) # store accuracy
+        test_stds_aux = torch.empty((1,11)) # store std
+        
         for j in range(11):
             accurate_aux = []
+            
             for i in range(10):
                 model = models[0]()
                 model.to(device)
             
                 train_loader, test_loader = load_data(N=1000, batch_size = models[4], seed=i)
+                # alpha = j/10
                 loss = train(model, train_loader, models[3], 0, 25, verbose=False, siamese=models[1], aux=models[2], alpha = j/10)
                 tr_accuracy, te_accuracy = accu(model, train_loader, test_loader, siamese=models[1], aux=models[2])
                 accurate_aux.append(te_accuracy)
             
             test_accuracies_aux[0,j] =  torch.FloatTensor(accurate_aux).mean()
             test_stds_aux[0,j] =  torch.FloatTensor(accurate_aux).std()
+            
         accuracy_aux.append(test_accuracies_aux)
         std_aux.append(test_stds_aux)
-        max_index = test_accuracies_aux.argmax() 
+        
+        max_index = test_accuracies_aux.argmax() # looking for maximum accuracy
         model_number_aux += 1
+        # Print out results
         print('The optimal alpha for aux_model_%d is %.2f' %(model_number_aux, max_index/10))
         
 
